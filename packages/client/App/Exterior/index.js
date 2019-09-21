63703733945b1423d49e77c51ff14575b101009e
Sharon Grossman <sharong1337@gmail.com>
2019-09-21 13:24:42 +0300

    refactor(exterior): changed app name and logo

--- packages/client/App/Exterior/index.js
+++ packages/client/App/Exterior/index.js
@@ -1,47 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Padded, Row, Column } from 'mui-flex-layout';
-import {
-  SchoolOutlined,
-  Code as CodeOutlined,
-  WebOutlined,
-  ComputerOutlined
-} from '@material-ui/icons';
+import { Row, Column, Padded } from 'mui-flex-layout';
+import { LocalShipping } from '@material-ui/icons';
 import { Typography } from '@material-ui/core';
 
-const logoSize = '75px !important';
+const logoSize = '150px !important';
 
-const School = styled(SchoolOutlined)`
-  font-size: ${logoSize};
-`;
-const Computer = styled(ComputerOutlined)`
-  font-size: ${logoSize};
-`;
-const Code = styled(CodeOutlined)`
-  font-size: ${logoSize};
-`;
-const Web = styled(WebOutlined)`
+const Construct = styled(LocalShipping)`
   font-size: ${logoSize};
 `;
 
 export default ({ children }) => (
   <Row width={'100%'} height={'100%'}>
-    <Column width={'100%'} justifyContent={'center'} alignItems={'center'}>
-      <Typography variant={'h5'}>{'makmacademy'}</Typography>
-      <Row justifyContent={'center'} alignItems={'center'} width={'100%'}>
-        <Padded padding={3}>
-          <School />
-        </Padded>
-        <Padded padding={3}>
-          <Web />
-        </Padded>
-        <Padded padding={3}>
-          <Computer />
-        </Padded>
-        <Padded padding={3}>
-          <Code />
-        </Padded>
-      </Row>
+    <Column width={'100%'} height={'75%'} justifyContent={'center'} alignItems={'center'}>
+      <Typography variant={'h3'}>{'Constrct.js'}</Typography>
+      <Padded>
+        <Typography variant={'body1'}>{'monorepo style application stack'}</Typography>
+      </Padded>
+      <Construct />
     </Column>
     <hr />
     <Column width={'100%'} height={'100%'}>
